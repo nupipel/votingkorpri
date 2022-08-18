bd6491a9df2962cd1a1dd5765854486b3bf90d6f
Rizki <rizkiwibowo333@gmail.com>
2022-08-18 15:55:46 +0700

    Candidate addColumn opd

--- app/Http/Controllers/Api/CandidateController.php
+++ app/Http/Controllers/Api/CandidateController.php
@@ -28,7 +28,7 @@ class CandidateController extends Controller
             array_push($names, $candidate->name);
             array_push($tvotes, $candidate->total_vote);
         }
-        $colors = ["#6571ff", "#7987a1", "#05a34a", "#66d1d1", "#fbbc06", "#ff3366", "#e9ecef", "#060c17", "#7987a1"];
+        $colors = ["#6571ff", "#05a34a", "#66d1d1", "#fbbc06", "#ff3366", "#e9ecef", "#060c17", "#7987a1"];
         for ($i = 0; $i < count($candidates); $i++) {
             array_push($dataColors, $colors[$i]);
         }
